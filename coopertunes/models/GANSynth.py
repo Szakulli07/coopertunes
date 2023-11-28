1cc41a7e1673ba2e63580a6cef76b4b022ffe9c5
TheAndreyy <andrzej.kusiak00@gmail.com>
2023-11-28 01:36:53 +0100

    feat: Adding GANSynth discriminator implementation

--- coopertunes/models/GANSynth.py
+++ coopertunes/models/GANSynth.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.nn import functional as F
 import torch
 
 from coopertunes.models import Model
@@ -8,6 +9,7 @@ EPS = 1e-8
 
 
 class Generator(nn.Module):
+    # FIXME: Padding to same and Upsample by kernel 2x2
 
     def __init__(self, hparams):
         super().__init__()
@@ -102,17 +104,80 @@ class PixelNormalization(nn.Module):
 class Discriminator(nn.Module):
 
     def __init__(self, hparams):
-        pass
+        super().__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(2, 32, 1, padding="same"),
+            nn.Conv2d(32, 32, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(32, 32, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(2)
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 64, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(2)
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 128, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(2)
+        )
+        self.block4 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(2)
+        )
+        self.block5 = nn.Sequential(
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(2)
+        )
+        self.block6 = nn.Sequential(
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(2)
+        )
+        self.block7 = nn.Sequential(
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding="same"),
+            nn.LeakyReLU(0.2),
+        )
+        self.pitch_classifier = nn.Linear(256 * 2 * 16, 61)
+        self.discriminator_output = nn.Linear(256 * 2 * 16, 1)
 
-    def forward(self, noise):
-        pass
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+        x = torch.flatten(x, 1)
+        dis_out = self.discriminator_output(x)
+        pitch = F.softmax(self.pitch_classifier(x))
+        return (dis_out, pitch)
 
 
 if __name__ == "__main__":
     batch_size = 16
     generator = Generator(None)
+    discriminator = Discriminator(None)
     noise = torch.randn(batch_size, 256)
     pitch = torch.zeros(batch_size, 61)
     pitch[:, 3] = 1
     x = generator(noise, pitch)
+    y = discriminator(x)
     i = 0
