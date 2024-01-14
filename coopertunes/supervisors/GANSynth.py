c534fd810cea6b6a426fef3156e3bc19298909fc
TheAndreyy <andrzej.kusiak00@gmail.com>
2024-01-14 14:12:13 +0100

    Adding pitch to GANSynth training

--- coopertunes/supervisors/GANSynth.py
+++ coopertunes/supervisors/GANSynth.py
@@ -19,6 +19,7 @@ class GANSynthSupervisor:
         self.generator_optimizer = Adam(self.generator.parameters(), lr=hparams.generator.lr, betas=hparams.generator.betas)
         self.discriminator_optimizer = Adam(self.discriminator.parameters(), lr=hparams.discriminator.lr, betas=hparams.discriminator.betas)
 
+        # TODO: real data
         self.train_loader = None
 
     def train(self):
@@ -27,34 +28,36 @@ class GANSynthSupervisor:
         self.generator.train()
         self.discriminator.train()
 
-        criterion = nn.MSELoss()
+        source_criterion = nn.BCEWithLogitsLoss()
+        pitch_criterion = nn.CrossEntropyLoss()
 
-        # TODO: Pitch
-        for i in range(self.hparams.epochs):
+        for epoch in range(self.hparams.epochs):
             for data in self.train_loader:
                 self.discriminator_optimizer.zero_grad()
                 real_images = data[0].to(self.device)
+                real_pitch = data[1].to(self.device)
                 batch_size = real_images.size(0)
                 label = torch.ones((batch_size,), dtype=torch.float, device=self.device)
-                output = self.discriminator(real_images).view(-1)
-                error_discriminator_real = criterion(output, label)
+                (discriminator_output, discriminator_pitch) = self.discriminator(real_images)                       # .view(-1)
+                error_discriminator_real = source_criterion(discriminator_output.view(-1), label) + pitch_criterion(discriminator_pitch, real_pitch)
                 error_discriminator_real.backward()
 
                 noise = torch.randn(batch_size, self.hparams.generator.latent_dim, device=self.device)
-                pitch = torch.zeros(batch_size, 61)
-                pitch[:, 3] = 1
-                fake_images = self.generator(noise, pitch)
+                # TODO: change to random pitch
+                fake_pitch = torch.zeros(batch_size, 61)
+                fake_pitch[:, 3] = 1
+                fake_images = self.generator(noise, fake_pitch)
                 label_fake = torch.zeros((batch_size,), dtype=torch.float, device=self.device)
-                output = self.discriminator(fake_images.detach())[0]
-                error_discriminator_fake = criterion(output, label_fake)
+                (discriminator_output, discriminator_pitch) = self.discriminator(fake_images.detach())
+                error_discriminator_fake = source_criterion(discriminator_output.view(-1), label_fake) + pitch_criterion(discriminator_pitch, fake_pitch)
                 error_discriminator_fake.backward()
                 error_discriminator = error_discriminator_real + error_discriminator_fake
                 self.discriminator_optimizer.step()
 
                 self.generator_optimizer.zero_grad()
                 label = torch.ones((batch_size,), dtype=torch.float, device=self.device)
-                output = self.discriminator(fake_images)[0]
-                error_generator = criterion(output, label)
+                (discriminator_output, discriminator_pitch) = self.discriminator(fake_images)
+                error_generator = source_criterion(discriminator_output.view(-1), label) + pitch_criterion(discriminator_pitch, fake_pitch)
                 error_generator.backward()
                 self.generator_optimizer.step()
 
