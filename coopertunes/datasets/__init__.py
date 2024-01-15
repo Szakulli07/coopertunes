fb898157bc93ff7b9fe9732a6b33d6e4c57a0f62
TheAndreyy <andrzej.kusiak00@gmail.com>
2024-01-15 16:01:53 +0100

    Making GANSynth classes public

--- coopertunes/datasets/__init__.py
+++ coopertunes/datasets/__init__.py
@@ -1,10 +1,11 @@
-from coopertunes.datasets.MelDataset import MelDataset
 from coopertunes.datasets.AudioDataset import AudioDataset
-from coopertunes.datasets.MidiDataset import MidiDataset
 from coopertunes.datasets.GANSynthDataset import GANSynthDataset
+from coopertunes.datasets.MelDataset import MelDataset
+from coopertunes.datasets.MidiDataset import MidiDataset
 
 __all__ = [
     "MelDataset",
     "AudioDataset",
-    "MidiDataset"
+    "MidiDataset",
+    "GANSynthDataset",
 ]
